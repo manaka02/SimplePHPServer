6f98c5cdcc0f22a4092f6668eccec6c0640a468d
toavina <toavinar@outlook.com>
2017-10-12 17:02:10 +0300

    in the middle of sync system

--- usertoken (2).sql
+++ usertoken (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mar 10 Octobre 2017 à 06:55
+-- Généré le: Jeu 12 Octobre 2017 à 14:01
 -- Version du serveur: 5.6.11-log
 -- Version de PHP: 5.4.15
 
@@ -30,28 +30,30 @@ USE `usertoken`;
 
 CREATE TABLE IF NOT EXISTS `account` (
   `id_account` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(50) DEFAULT NULL,
-  `father` varchar(50) DEFAULT NULL,
-  `idmobile` varchar(100) DEFAULT NULL,
-  `exptoken` varchar(100) DEFAULT NULL,
-  `connected` smallint(6) DEFAULT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `alias` varchar(60) NOT NULL,
+  `idmobile` varchar(100) NOT NULL,
+  `exptoken` varchar(100) NOT NULL,
+  `connected` smallint(6) NOT NULL,
   PRIMARY KEY (`id_account`),
   UNIQUE KEY `exptoken` (`exptoken`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf32 AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf32 AUTO_INCREMENT=13 ;
 
 --
 -- Contenu de la table `account`
 --
 
-INSERT INTO `account` (`id_account`, `pseudo`, `father`, `idmobile`, `exptoken`, `connected`) VALUES
-(1, 'toavina', NULL, 'sony', 'expSony', 0),
-(2, 'miora', NULL, 'htc', 'expohtc', 1),
-(3, 'toavina', '1', 'zte', 'expzte', 1),
-(4, 'miora', '2', 'xiaomi', 'expmi', 1),
-(6, 'toavina', '1', 'expSony', NULL, 1),
-(7, 'toavina', NULL, NULL, 'ExpXuaoi', 1),
-(8, 'miora', '2', 'Siemens', 'expSiemens', 1),
-(9, 'miora', '2', 'Siemens', 'expSiemen', 1);
+INSERT INTO `account` (`id_account`, `pseudo`, `alias`, `idmobile`, `exptoken`, `connected`) VALUES
+(1, 'toavina', 'toavina', 'sony', 'expSony', 0),
+(2, 'miora', 'miora', 'zte', 'expZTE', 1),
+(4, 'toavina', 'marchand2', 'sonyoooh', 'expSonnyyy', 1),
+(5, 'toavina', 'mrchand3', 'snny', 'espsony', 1),
+(7, 'miora', 'mrchand1', '', 'ExponentPushToken[Ry2QvANEDLZLfcwRQnd9R7]', 1),
+(8, 'toavina', 'marchnd4', 'sony4', 'ExponentPushToken[Ry2QvANEDLZLfcwRQnd9RFG]', 0),
+(9, 'toavina', 'marchand5', 'sony2', 'ExponentPushToken[Ry2QvANEDLZLfcwRQnd9R4]', 0),
+(12, 'toavina', 'caisse3', 'sonyXperia ZR', 'ExponentPushToken[Ry2QvANEDLZLfcwRQnd9Ro]', 1),
+(10, 'toavina', 'caisse1', 'sony', 'ExponentPushToken[Ry2QvANEDLZLfcwRQnd9RFGsdfg]', 0),
+(11, 'toavina', 'caisse2', 'sony2', 'ExponentPushToken[Ry2QvANEDLZLfcwRQnd9R4df]', 0);
 
 -- --------------------------------------------------------
 
@@ -99,20 +101,35 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   PRIMARY KEY (`id_transaction`),
   KEY `fk_recipient` (`recipient`),
   KEY `fk_sender` (`sender`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf32 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tree`
+--
+
+CREATE TABLE IF NOT EXISTS `tree` (
+  `id_tree` int(11) NOT NULL AUTO_INCREMENT,
+  `id_account` int(11) NOT NULL,
+  `father` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_tree`),
+  KEY `fk_account` (`id_account`),
+  KEY `fk_pere` (`father`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf32 AUTO_INCREMENT=8 ;
 
 --
--- Contenu de la table `transaction`
+-- Contenu de la table `tree`
 --
 
-INSERT INTO `transaction` (`id_transaction`, `sender`, `recipient`, `currency`, `amount`, `date_transaction`, `type`, `comment`) VALUES
-(1, 3, 2, 'Ar', 250, '2017-10-11 00:00:00', 'payement', NULL),
-(2, 3, 2, 'Ar', 1250, '2017-10-11 00:00:00', 'payement', NULL),
-(3, 4, 1, 'Ar', 250, '2017-10-11 00:00:00', 'payement', NULL),
-(4, 4, 1, 'Ar', 2150, '2017-10-11 00:00:00', 'payement', NULL),
-(5, 1, 4, 'Ar', 250, '2017-10-11 00:00:00', 'payement', NULL),
-(6, 1, 2, 'Ar', 250, '2017-10-11 00:00:00', 'payement', NULL),
-(7, 3, 4, 'Ar', 1500, NULL, 'expSiemen', 'comment');
+INSERT INTO `tree` (`id_tree`, `id_account`, `father`) VALUES
+(1, 11, 1),
+(2, 12, 1),
+(3, 12, 1),
+(4, 12, 1),
+(5, 12, 1),
+(6, 12, 1),
+(7, 12, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
